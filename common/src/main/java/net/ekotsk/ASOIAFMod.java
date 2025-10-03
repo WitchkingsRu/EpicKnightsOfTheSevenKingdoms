668b6f5cb9e1887b68177a122a904a6de5df7e84
WitchkingsRu <vovakla647@gmail.com>
2025-10-03 22:56:03 +0300

    initial upload

---
+++ common/src/main/java/net/ekotsk/ASOIAFMod.java
@@ -0,0 +1,9 @@
+package net.ekotsk;
+
+public final class ASOIAFMod {
+    public static final String MOD_ID = "ekotsk";
+
+    public static void init() {
+        System.out.println("penis");
+    }
+}
---
+++ fabric/src/main/java/net/ekotsk/fabric/ASOIAFModFabric.java
@@ -0,0 +1,17 @@
+package net.ekotsk.fabric;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.ekotsk.ASOIAFMod;
+
+public final class ASOIAFModFabric implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        // This code runs as soon as Minecraft is in a mod-load-ready state.
+        // However, some things (like resources) may still be uninitialized.
+        // Proceed with mild caution.
+
+        // Run our common setup.
+        ASOIAFMod.init();
+    }
+}
