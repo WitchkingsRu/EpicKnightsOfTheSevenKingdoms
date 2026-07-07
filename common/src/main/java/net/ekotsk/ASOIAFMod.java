e1c80bd50989efc489a3401846b750b467d77857
WitchkingsRu <vovakla647@gmail.com>
2026-07-07 22:57:23 +0300

    initial commit

--- common/src/main/java/net/ekotsk/ASOIAFMod.java
+++ common/src/main/java/net/ekotsk/ASOIAFMod.java
@@ -1,6 +1,6 @@
 package net.ekotsk;
 
-public final class ExampleMod {
+public final class ASOIAFMod {
     public static final String MOD_ID = "ekotsk";
 
     public static void init() {
---
+++ neoforge/src/main/java/net/ekotsk/neoforge/ASOIAFModNeoForge.java
@@ -0,0 +1,13 @@
+package net.ekotsk.neoforge;
+
+import net.neoforged.fml.common.Mod;
+
+import net.ekotsk.ASOIAFMod;
+
+@Mod(ASOIAFMod.MOD_ID)
+public final class ASOIAFModNeoForge {
+    public ASOIAFModNeoForge() {
+        // Run our common setup.
+        ASOIAFMod.init();
+    }
+}
