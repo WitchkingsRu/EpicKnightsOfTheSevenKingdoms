17e3dc81dc3f1bf6ef570f4f0237a7cb55210abf
WitchkingsRu <vovakla647@gmail.com>
2026-08-19 21:13:14 +0300

    pre-release preparations

--- common/src/main/java/net/ekotsk/misc/ModMerchantOffers.java
+++ common/src/main/java/net/ekotsk/misc/ModMerchantOffers.java
@@ -45,10 +45,21 @@ public class ModMerchantOffers {
                 new SimpleTrade(new ItemCost(Items.EMERALD), Optional.empty(), new ItemStack(ModItems.HORN_PATTERN.get()), 7, 0, 1),
                 new SimpleTrade(new ItemCost(Items.EMERALD), Optional.empty(), new ItemStack(ModItems.BONEHAND_PATTERN.get()), 7, 0, 1),
                 new SimpleTrade(new ItemCost(Items.EMERALD), Optional.empty(), new ItemStack(ModItems.SEVEN_PATTERN.get()), 7, 0, 1),
+                new SimpleTrade(new ItemCost(Items.EMERALD), Optional.empty(), new ItemStack(ModItems.WYL_PATTERN.get()), 7, 0, 1),
+                new SimpleTrade(new ItemCost(Items.EMERALD), Optional.empty(), new ItemStack(ModItems.HOG_PATTERN.get()), 7, 0, 1),
+                new SimpleTrade(new ItemCost(Items.EMERALD), Optional.empty(), new ItemStack(ModItems.SHIELD_PATTERN.get()), 7, 0, 1),
+                new SimpleTrade(new ItemCost(Items.EMERALD), Optional.empty(), new ItemStack(ModItems.APPLE_PATTERN.get()), 7, 0, 1),
+                new SimpleTrade(new ItemCost(Items.EMERALD), Optional.empty(), new ItemStack(ModItems.HIVES_PATTERN.get()), 7, 0, 1),
+                new SimpleTrade(new ItemCost(Items.EMERALD), Optional.empty(), new ItemStack(ModItems.HUNTER_PATTERN.get()), 7, 0, 1),
+                new SimpleTrade(new ItemCost(Items.EMERALD), Optional.empty(), new ItemStack(ModItems.HORSE_HEAD_PATTERN.get()), 7, 0, 1),
+                new SimpleTrade(new ItemCost(Items.EMERALD), Optional.empty(), new ItemStack(ModItems.PRANCING_HORSE_PATTERN.get()), 7, 0, 1),
+                new SimpleTrade(new ItemCost(Items.EMERALD), Optional.empty(), new ItemStack(ModItems.CIRCLES_PATTERN.get()), 7, 0, 1),
+                new SimpleTrade(new ItemCost(Items.EMERALD), Optional.empty(), new ItemStack(ModItems.TWO_TOWERS_PATTERN.get()), 7, 0, 1),
+                new SimpleTrade(new ItemCost(Items.EMERALD), Optional.empty(), new ItemStack(ModItems.THREE_TOWERS_PATTERN.get()), 7, 0, 1),
         };
 
         WANDERING_TRADER_TRADES = new VillagerTrades.ItemListing[] {
-                new SimpleTrade(new ItemCost(Items.EMERALD), Optional.empty(), new ItemStack(ModItems.GOLD_CLOAK_RIGHT_DECORATION.get()), 1, 0, 1),
+                new SimpleTrade(new ItemCost(Items.EMERALD), Optional.empty(), new ItemStack(ModItems.GOLD_CLOAK_RIGHT_DECORATION.get()), 5, 0, 1),
                 new SimpleTrade(new ItemCost(Items.EMERALD), Optional.empty(), new ItemStack(ModItems.GOLD_CLOAK_LEFT_DECORATION.get()), 5, 0, 1),
                 new SimpleTrade(new ItemCost(Items.EMERALD), Optional.empty(), new ItemStack(ModItems.FANCY_CAPE_DECORATION.get()), 5, 0, 1),
                 new SimpleTrade(new ItemCost(Items.EMERALD), Optional.empty(), new ItemStack(ModItems.DRAGON_DECORATION.get()), 5, 0, 1),
